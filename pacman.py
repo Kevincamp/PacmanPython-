b8efc601d1d7d336e048c401af6d68a0b64ffbbc
KevinCamp <kevincamp_90@me.com>
2015-05-30 11:54:36 -0500

    Animación de Pacman
    Por el momento animación de Pacman en una sola dirección. Solo
    horizontal.

--- pacman.py
+++ pacman.py
@@ -3,10 +3,11 @@
 
 # Módulos
 import sys
+import glob
 import time
 import pygame
 import random
-import Image
+#import Image
 from pygame.locals import * 
 # Constantes
 WIDTH = 640
@@ -14,15 +15,16 @@ HEIGHT = 480
 SPEED = 0.3
 BORDES = 3
 activado = 0
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 # Clases
 # ---------------------------------------------------------------------
 
 class Galleta(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = load_image("./Imagenes/galleta.png", True)
+        self.image = load_image("./Imagenes/cookie.png", True)
         self.rect = self.image.get_rect()
-        print self.rect.width,self.rect.width
+        #print self.rect.width,self.rect.width
         self.rect.centerx = WIDTH/2
         self.rect.centery = HEIGHT/2  
 
@@ -30,12 +32,18 @@ class Galleta(pygame.sprite.Sprite):
 class Pacman(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = load_image("./Imagenes/pacman3.png", True)
+        self.ani = glob.glob("./Imagenes/moviendose/pac*.png")
+        self.ani.sort()
+        self.ani_pos = 0
+        self.ani_max = len(self.ani) - 1
+        self.image = load_image(self.ani[0], True)
         self.rect = self.image.get_rect()
-        print self.rect.width,self.rect.width
+        print self.rect.width,self.rect.height
         self.rect.centerx = BORDES + self.rect.width/2
         self.rect.centery = HEIGHT - self.rect.height/2 - BORDES
         self.speed = [0, 0]
+        self.actualizarMovimiento(0)
+        
     def actualizar(self, time):
         self.rect.centerx += self.speed[0] * time
         self.rect.centery += self.speed[1] * time
@@ -45,6 +53,16 @@ class Pacman(pygame.sprite.Sprite):
         if self.rect.top <= 0 or self.rect.bottom >= HEIGHT:
             self.speed[1] = -self.speed[1]
             self.rect.centery += self.speed[1] * time
+            
+    def actualizarMovimiento(self,pos):
+        if pos != 0:
+            if self.speed != [0,0]:
+                self.image = load_image(self.ani[self.ani_pos],True)
+                if self.ani_pos == self.ani_max:
+                    self.ani_pos=0
+                else:
+                    self.ani_pos+=1
+                    
     def mover(self, time, keys):
         if self.rect.top >= 0:
             if keys[K_UP]:
@@ -83,8 +101,8 @@ def load_image(filename, transparent=False):
 
 #----------------------------------------------------------------------
 def main():
+    pos = 0
     activado = 0
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
     pygame.display.set_caption("Prueba de ventana")
     pacman = Pacman()
     galleta = Galleta()
@@ -95,19 +113,18 @@ def main():
     while True:
         keys = pygame.key.get_pressed()
         time = clock.tick(60)
+        screen.fill((0,0,0))
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
-                pygame.QUIT()
                 sys.exit()
-        screen.fill((0,0,0))
-        #pacman.actualizar(time)
         if keys[K_UP] or keys[K_DOWN] or keys[K_LEFT] or keys[K_RIGHT] and activado == 0:
             pygame.mixer.music.play(-1)
             activado = 1
+            pos = 1
         pacman.mover(time,keys)
+        pacman.actualizarMovimiento(pos)
         screen.blit(galleta.image, galleta.rect)
         screen.blit(pacman.image, pacman.rect)
-
         pygame.display.update()
     return 0
 #---------------------------------------------------------------------------------------------
