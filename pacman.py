6baa7b93185142ac97e53fbed84ac4fe5d4b1e64
razerjon24 <razerjon24@gmail.com>
2015-06-06 18:19:50 -0500

    Agregada clase Laberinto funcional

--- pacman.py
+++ pacman.py
@@ -18,41 +18,91 @@ activado = 0
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 # Clases
 # ---------------------------------------------------------------------
-class Laberinto(pygame.sprite.Sprite):
+class Laberinto():
+    mazeWidth = 0
+    mazeHeight = 0
     def __init__(self,width,height):
-        pygame.sprite.Sprite.__init__(self)
-        laberintoParteIZQ = [[0 for _ in range((height))] for _ in range((width/2))]
-        #insertar funcion para crear laberinto parte IZQ
-        laberintoParteDER = espejo(laberintoParteIZQ, width, height)
-        laberintoMatriz = fusion(laberintoParteIZQ,laberintoParteDER,width,height)
+        self.mazeWidth = width
+        self.mazeHeight = height
         
+    def getMaze(self):
+        laberinto = [[0 for _ in range((self.mazeWidth))] for _ in range((self.mazeHeight))]
+        laberinto = self.marcoLaberinto(laberinto,self.mazeHeight,self.mazeWidth)
+        laberinto = self.generacionAleatoria(laberinto,self.mazeHeight,self.mazeWidth)
+        return laberinto
     
-    def espejo(laberintoParteIZQ,height,width):
-        laberintoParteDER = [[0 for _ in range((height))] for _ in range((width/2))]
-        mirrorContj = width/2 - 1
-        for i in range(0,height):
-            for j in range(0,width/2):
-                 laberintoParteDER[i][mirrorContj] = laberintoParteIZQ[i][j]
-                 mirrorContj -= 1
-            mirrorContj = width/2 - 1
-        return laberintoParteDER
+    def generacionAleatoria(self,laberinto, height, width):
+        muroInitSize = random.randint(1,width/2)
+        muroInitList = [0 for _ in range(muroInitSize)]
+        muroPos = [2,4]
+        for x in range (0,muroInitSize):
+            if(x != 0):
+                muroX = random.choice(muroPos)
+                if(muroX+muroInitList[x-1] < width - 1):
+                    muroInitList[x]=muroX
+                    laberinto[1][muroX] = 1
+            else:
+                muroX = random.choice([1,2])
+                muroInitList[x]=muroX
+                laberinto[1][muroX] = 1
+        for i in range(2,height-1):
+            for j in range(1,width-1):
+                #izquierdoconMarco
+                if(j == 1):
+                    if(laberinto[i][j] == 0 and laberinto[i-1][j] == 0 and laberinto[i-1][j+1] == 0 and laberinto[i][j+1] == 0):
+                        laberinto[i][j] = 1
+                #derechoconMarco
+                elif(j == width-2):
+                    if(laberinto[i][j]==0 and laberinto[i][j-1] == 0 and laberinto[i-1][j-1] == 0 and laberinto[i-1][j] == 0):
+                        laberinto[i][j] = 1
+                #abajoconMarco    
+                elif(i == height-2 and j>1 and j<width-2):
+                    if(laberinto[i][j]==0 and laberinto[i][j-1]==0 and laberinto[i-1][j-1]==0 and laberinto[i-1][j]==0 and laberinto[i-1][j+1]==0 and laberinto[i][j+1]==0):
+                        laberinto[i][j] = 1
+                        self.ponerMuro(laberinto,i,j,"abajo")
+                else:
+                    if(laberinto[i][j]==0 and laberinto[i][j-1]==0 and laberinto[i-1][j-1]==0 and laberinto[i-1][j]==0 and laberinto[i-1][j+1]==0 and laberinto[i][j+1]==0):
+                        laberinto[i][j] = 1
+                        self.ponerMuro(laberinto,i,j,"centro")
+                    elif(laberinto[i][j]==0 and laberinto[i][j-1]==0 and laberinto[i-1][j-1]==0 and laberinto[i-1][j]==0):
+                        laberinto[i][j] = 1
+        return laberinto
     
-    def fusion(laberintoParteIZQ, laberintoParteDER, height, width):
-        laberintoMatriz = [[0 for _ in range((height))] for _ in range((width))]
-        contizqj = 0
-        contderj = 0
+    def ponerMuro(self,laberinto,i,j,estado):
+        #pickDR{down,right,downright}
+        if(estado == "centro"):
+            pickDR = random.randint(1,3)
+            if(pickDR == 1):
+                laberinto[i+1][j] = 1
+            elif(pickDR == 2):
+                if(self.espaciosBlancoMuro(laberinto,i,j+1)):
+                    laberinto[i][j+1] = 1
+            elif(pickDR == 3):
+                if(self.espaciosBlancoMuro(laberinto,i,j+1)):
+                    laberinto[i][j-1] = 1
+                    laberinto[i+1][j] = 1
+            return 0
+        elif(estado == "abajo"):
+            pickDR = random.randint(0,1)
+            if(pickDR == 1):
+                if(self.espaciosBlancoMuro(laberinto,i,j+1)):
+                    laberinto[i][j+1] = 1
+            return 0
+            
+    def marcoLaberinto(self,laberinto, height, width):
         for i in range(0,height):
             for j in range(0,width):
-                #recorrido parte IZQ
-                if(j>=0 and j<width/2):
-                    laberintoMatriz[i][j] = laberintoParteIZQ[i][contizqj]
-                    contizqj += 1
-                else:
-                    laberintoMatriz[i][j] = laberintoParteDER[i][contderj]    
-                    contderj += 1
-            contizqj = 0
-            contderj = 0
-        return laberintoMatriz
+                if(i==0 or i==height-1):
+                    laberinto[i][j] = 1
+                elif(j==0 or j==width-1):
+                    laberinto[i][j] = 1
+        return laberinto
+    
+    def espaciosBlancoMuro(self,laberinto,i,j):
+        if(laberinto[i-1][j+1] == 0 and laberinto[i][j+1] == 0):
+            return True
+        else:
+            return False
         
         
 # ---------------------------------------------------------------------
@@ -176,7 +226,7 @@ def main():
     pygame.mixer.music.load("./Sound/pacman_chomp.wav")
     keys = pygame.key.get_pressed()
     clock = pygame.time.Clock()
-    
+    #maze = Laberinto(WIDTH,HEIGHT)
     def comerGalleta(x1,y1,w1,h1,x2,y2,w2,h2):
         if (x2+w2>=x1>=x2 and y2+h2>=y1>=y2):
             return True    
